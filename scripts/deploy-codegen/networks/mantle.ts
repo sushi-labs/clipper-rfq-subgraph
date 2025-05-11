cd2c3902a16a68ebe6f23c82a65c2bd1dba4ac7e
Daniel Reinoso <daniel@sushi.com>
2025-05-10 20:57:40 -0400

    format network configs

--- scripts/deploy-codegen/networks/mantle.ts
+++ scripts/deploy-codegen/networks/mantle.ts
@@ -4,60 +4,60 @@ export function getMantleDeploymentConfig(): Deployment {
   return {
     networkName: 'mantle',
     pools: [
-        {
-          address: '0x769728b5298445BA2828c0f3F5384227fbF590C5',
-          startBlock: 5192720,
-          permitRouter: '0x98898Bc1975e369345519b6C11c9A1F8A37AB877',
-          contractAbiName: 'ClipperPackedVerifiedExchange',
-          sourceAbi: 'ClipperCommonExchangeV0',
-        },
-      ],
-      coves: [], // No coves for mantle yet
-      addressZeroMap: {
-        symbol: 'MNT',
-        decimals: 18,
-        name: 'Mantle',
-        address: '0x0000000000000000000000000000000000000000',
+      {
+        address: '0x769728b5298445BA2828c0f3F5384227fbF590C5',
+        startBlock: 5192720,
+        permitRouter: '0x98898Bc1975e369345519b6C11c9A1F8A37AB877',
+        contractAbiName: 'ClipperPackedVerifiedExchange',
+        sourceAbi: 'ClipperCommonExchangeV0',
       },
+    ],
+    coves: [], // No coves for mantle yet
+    addressZeroMap: {
+      symbol: 'MNT',
+      decimals: 18,
+      name: 'Mantle',
+      address: '0x0000000000000000000000000000000000000000',
+    },
 
-      prune: Math.floor((1.5 * 30 * 24 * 60 * 60) / 2), // 1.5 months of blocks with 2s block time
+    prune: Math.floor((1.5 * 30 * 24 * 60 * 60) / 2), // 1.5 months of blocks with 2s block time
 
-      priceOracles: [
-        {
-          tokens: ['0xdEAddEaDdeadDEadDEADDEAddEADDEAddead1111'],
-          address: '0x5bc7Cf88EB131DB18b5d7930e793095140799aD5',
-          startBlock: 74405378,
-          contractAbiName: 'AggregatorV3Interface',
-          sourceAbi: 'AggregatorV3Interface',
-        },
-        {
-          tokens: ['0xCAbAE6f6Ea1ecaB08Ad02fE02ce9A44F09aebfA2'],
-          address: '0x7db2275279F52D0914A481e14c4Ce5a59705A25b',
-          startBlock: 74402393,
-          contractAbiName: 'AggregatorV3Interface',
-          sourceAbi: 'AggregatorV3Interface',
-        },
-        {
-          tokens: ['0x09Bc4E0D864854c6aFB6eB9A9cdF58aC190D0dF9'],
-          address: '0x22b422CECb0D4Bd5afF3EA999b048FA17F5263bD',
-          startBlock: 74405888,
-          contractAbiName: 'AggregatorV3Interface',
-          sourceAbi: 'AggregatorV3Interface',
-        },
-        {
-          tokens: ['0x201EBa5CC46D216Ce6DC03F6a759e8E766e956aE'],
-          address: '0xd86048D5e4fe96157CE03Ae519A9045bEDaa6551',
-          startBlock: 74406306,
-          contractAbiName: 'AggregatorV3Interface',
-          sourceAbi: 'AggregatorV3Interface',
-        },
-        {
-          tokens: ['0x78c1b0C915c4FAA5FffA6CAbf0219DA63d7f4cb8'],
-          address: '0xD97F20bEbeD74e8144134C4b148fE93417dd0F96',
-          startBlock: 74406588,
-          contractAbiName: 'AggregatorV3Interface',
-          sourceAbi: 'AggregatorV3Interface',
-        },
-      ],
+    priceOracles: [
+      {
+        tokens: ['0xdEAddEaDdeadDEadDEADDEAddEADDEAddead1111'],
+        address: '0x5bc7Cf88EB131DB18b5d7930e793095140799aD5',
+        startBlock: 74405378,
+        contractAbiName: 'AggregatorV3Interface',
+        sourceAbi: 'AggregatorV3Interface',
+      },
+      {
+        tokens: ['0xCAbAE6f6Ea1ecaB08Ad02fE02ce9A44F09aebfA2'],
+        address: '0x7db2275279F52D0914A481e14c4Ce5a59705A25b',
+        startBlock: 74402393,
+        contractAbiName: 'AggregatorV3Interface',
+        sourceAbi: 'AggregatorV3Interface',
+      },
+      {
+        tokens: ['0x09Bc4E0D864854c6aFB6eB9A9cdF58aC190D0dF9'],
+        address: '0x22b422CECb0D4Bd5afF3EA999b048FA17F5263bD',
+        startBlock: 74405888,
+        contractAbiName: 'AggregatorV3Interface',
+        sourceAbi: 'AggregatorV3Interface',
+      },
+      {
+        tokens: ['0x201EBa5CC46D216Ce6DC03F6a759e8E766e956aE'],
+        address: '0xd86048D5e4fe96157CE03Ae519A9045bEDaa6551',
+        startBlock: 74406306,
+        contractAbiName: 'AggregatorV3Interface',
+        sourceAbi: 'AggregatorV3Interface',
+      },
+      {
+        tokens: ['0x78c1b0C915c4FAA5FffA6CAbf0219DA63d7f4cb8'],
+        address: '0xD97F20bEbeD74e8144134C4b148fE93417dd0F96',
+        startBlock: 74406588,
+        contractAbiName: 'AggregatorV3Interface',
+        sourceAbi: 'AggregatorV3Interface',
+      },
+    ],
   }
 }
--- scripts/deploy-codegen/networks/polygon-zkevm.ts
+++ scripts/deploy-codegen/networks/polygon-zkevm.ts
@@ -5,60 +5,60 @@ export function getPolygonZkevmDeploymentConfig(): Deployment {
     networkName: 'polygon-zkevm',
     prune: Math.floor((1.5 * 30 * 24 * 60 * 60) / 3.2), // 1.5 months of blocks with 3.2s block time
 
-      pools: [
-        {
-          address: '0xae00af61be6861ee956c8e56bf22144d024acb57',
-          startBlock: 16665923,
-          contractAbiName: 'ClipperPackedOracleVerifiedExchange',
-          sourceAbi: 'ClipperCommonExchangeV0',
-        },
-        {
-          address: '0xe38c90a0233f18749fb74e595c4de871e5498c13',
-          startBlock: 15320389,
-          contractAbiName: 'ClipperPackedOracleVerifiedExchange',
-          sourceAbi: 'ClipperCommonExchangeV0',
-        },
-      ],
-
-      coves: [
-        {
-          address: '0x097Bf4a933747679698A97A9145Ce2c7f3c46042',
-          startBlock: 16665927,
-          contractAbiName: 'ClipperCove',
-          poolContractAbiName: 'ClipperPackedOracleVerifiedExchange',
-          sourceAbi: 'ClipperCove',
-        },
-      ],
-
-      addressZeroMap: {
-        symbol: 'ETH',
-        decimals: 18,
-        name: 'Ether',
-        address: '0x0000000000000000000000000000000000000000',
+    pools: [
+      {
+        address: '0xae00af61be6861ee956c8e56bf22144d024acb57',
+        startBlock: 16665923,
+        contractAbiName: 'ClipperPackedOracleVerifiedExchange',
+        sourceAbi: 'ClipperCommonExchangeV0',
       },
+      {
+        address: '0xe38c90a0233f18749fb74e595c4de871e5498c13',
+        startBlock: 15320389,
+        contractAbiName: 'ClipperPackedOracleVerifiedExchange',
+        sourceAbi: 'ClipperCommonExchangeV0',
+      },
+    ],
 
-      priceOracles: [
-        {
-          tokens: ['0x22B21BedDef74FE62F031D2c5c8F7a9F8a4b304D'],
-          address: '0x44285b60Cc13557935CA4945d20475BD1f1058f4',
-          startBlock: 15743026,
-          contractAbiName: 'AggregatorV3Interface',
-          sourceAbi: 'AggregatorV3Interface',
-        },
-        {
-          tokens: ['0xa2036f0538221a77A3937F1379699f44945018d0'],
-          address: '0x7C85dD6eBc1d318E909F22d51e756Cf066643341',
-          startBlock: 4931870,
-          contractAbiName: 'AggregatorV3Interface',
-          sourceAbi: 'AggregatorV3Interface',
-        },
-        {
-          tokens: ['0x37eAA0eF3549a5Bb7D431be78a3D99BD360d19e5'],
-          address: '0x0167D934CB7240e65c35e347F00Ca5b12567523a',
-          startBlock: 4931982,
-          contractAbiName: 'AggregatorV3Interface',
-          sourceAbi: 'AggregatorV3Interface',
-        },
-      ],
+    coves: [
+      {
+        address: '0x097Bf4a933747679698A97A9145Ce2c7f3c46042',
+        startBlock: 16665927,
+        contractAbiName: 'ClipperCove',
+        poolContractAbiName: 'ClipperPackedOracleVerifiedExchange',
+        sourceAbi: 'ClipperCove',
+      },
+    ],
+
+    addressZeroMap: {
+      symbol: 'ETH',
+      decimals: 18,
+      name: 'Ether',
+      address: '0x0000000000000000000000000000000000000000',
+    },
+
+    priceOracles: [
+      {
+        tokens: ['0x22B21BedDef74FE62F031D2c5c8F7a9F8a4b304D'],
+        address: '0x44285b60Cc13557935CA4945d20475BD1f1058f4',
+        startBlock: 15743026,
+        contractAbiName: 'AggregatorV3Interface',
+        sourceAbi: 'AggregatorV3Interface',
+      },
+      {
+        tokens: ['0xa2036f0538221a77A3937F1379699f44945018d0'],
+        address: '0x7C85dD6eBc1d318E909F22d51e756Cf066643341',
+        startBlock: 4931870,
+        contractAbiName: 'AggregatorV3Interface',
+        sourceAbi: 'AggregatorV3Interface',
+      },
+      {
+        tokens: ['0x37eAA0eF3549a5Bb7D431be78a3D99BD360d19e5'],
+        address: '0x0167D934CB7240e65c35e347F00Ca5b12567523a',
+        startBlock: 4931982,
+        contractAbiName: 'AggregatorV3Interface',
+        sourceAbi: 'AggregatorV3Interface',
+      },
+    ],
   }
 }
